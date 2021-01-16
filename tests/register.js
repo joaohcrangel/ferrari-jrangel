af233585e0ef46ccaddac4c3c048984a64c14859
João Rangel <joaohcrangel@gmail.com>
2021-01-16 00:25:23 -0300

    ok

--- tests/register.js
+++ tests/register.js
@@ -1,3 +1,11 @@
+const { format } = require('date-fns')
+const faker = require('faker')
+
+const name = faker.name.firstName() + ' ' + faker.name.lastName()
+const email = faker.internet.email().toLowerCase()
+const birthDate = format(faker.date.past(), 'yyyy-MM-dd')
+const password = faker.internet.password()
+
 module.exports = {
     'PASSO 1: Acessar a página de autenticação': function(browser) {
 
@@ -15,12 +23,32 @@ module.exports = {
 
         browser
             .waitForElementVisible('body')
-            .assert.visible('#auth-email > div.actions > a')
-            .click('#auth-email > div.actions > a')
-            .pause(3000)
+            .assert.visible('#login > div.actions > div > a:nth-child(2)')
+            .click('#login > div.actions > div > a:nth-child(2)')
+            .assert.visible('#register')
+            .setValue('#email-register', email)
+            .setValue('#name', name)
+            .setValue('#birth_at', birthDate)
+            .setValue('#password-new', password)
+            .setValue('#password-confirm', password)
+            .saveScreenshot('./tests/screenshots/formulario-preenchido.png')
+            .assert.visible('#register > div.actions > button')
+            .click('#register > div.actions > button')
+            .pause(1000)
 
     },
     'PASSO 3: Verficar se autenticou após o cadastro': function(browser) {
+
+        browser
+            .waitForElementVisible('body')
+            .assert.urlEquals('http://localhost:8080/')
+            .assert.visible('#header > div.menu.logged > div > div > div > a > small')
+            .saveScreenshot('./tests/screenshots/home-autenticada.png')
+            .assert.containsText('#header > div.menu.logged > div > div > div > a > small', email)
+            .pause(3000)
+            .end()
+
+
 
     },
 }
